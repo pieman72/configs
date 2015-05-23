1cb67303f109e2fc0867fead1c06609b15cc2139
pieman72 <pieman72@gmail.com>
2015-05-23 18:17:23 -0400

    Adding more mail stuff

--- export.sh
+++ export.sh
@@ -72,6 +72,10 @@ prompt_replace "$DIR/tmux_tempr.sh" "$HOME/scripts/tmux_tempr.sh"
 
 # Mutt
 echo -e "\n\e[38;5;208m--- Exporting mutt configs\e[39m"
+echo "mail"
+if [ -z "$(which mail)" ]; then sudo apt-get install mail; fi
+echo "mutt"                                              
+if [ -z "$(which mutt)" ]; then sudo apt-get install mutt; fi
 echo ".muttrc"
 prompt_replace "$DIR/.muttrc" "$HOME/.muttrc"
 echo ".mailcap"
